0c627fa55351c6bf9bc1e03274aa495cd5af210d
j.lee <sdf090400@gmail.com>
2025-11-26 13:33:54 +0900

    이거진짜 통장내역넣어보았습니다.

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.transaction_list, name='transaction_list'),
     path('delete/<int:pk>/', views.transaction_delete, name='transaction_delete'),
+    path('update/<int:pk>/', views.transaction_list, name='transaction_update'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -3,35 +3,42 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from .models import Transaction
 from .forms import TransactionForm
-
+#pk일때수정그외 기본으로수정불가함으로만듬
 @login_required(login_url='/admin/login/')
-def transaction_list(request):
-    # [1] 생성 로직 (POST 요청 시)
+def transaction_list(request, pk=None):
+
+    if pk:
+        target_transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    else:
+        target_transaction = None
+
+
     if request.method == 'POST':
-        form = TransactionForm(request.POST)
+
+        form = TransactionForm(request.POST, instance=target_transaction)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.user = request.user # 로그인 사용자 자동 연결
+            transaction.user = request.user
             transaction.save()
             return redirect('transaction_list')
-    else:
-        form = TransactionForm()
 
-    # [2] 조회 및 필터링 로직 (GET 요청 시)
-    # 로그인한 사용자의 데이터만 가져옵니다.
+
+    else:
+
+        form = TransactionForm(instance=target_transaction)
+
+
     transactions = Transaction.objects.filter(user=request.user)
+
 
     search_type = request.GET.get('search_type')
     search_query = request.GET.get('search_query')
-
     if search_type:
         transactions = transactions.filter(type=search_type)
-
     if search_query:
-        # memo 필드 검색
         transactions = transactions.filter(memo__icontains=search_query)
 
-    # [3] 상단 요약 카드 계산 (전체 데이터 기준)
+
     my_data = Transaction.objects.filter(user=request.user)
     total_income = my_data.filter(type='INCOME').aggregate(Sum('amount'))['amount__sum'] or 0
     total_expense = my_data.filter(type='EXPENSE').aggregate(Sum('amount'))['amount__sum'] or 0
@@ -43,16 +50,14 @@ def transaction_list(request):
         'total_income': total_income,
         'total_expense': total_expense,
         'balance': balance,
+        'update_pk': pk,
     }
-
 
     return render(request, 'transactions/transaction_list.html', context)
 
 @login_required
 def transaction_delete(request, pk):
-
     if request.method == 'POST':
-
         transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
         transaction.delete()
     return redirect('transaction_list')
